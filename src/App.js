d7f7782206d9516781101904f61c3169c58fd2f3
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-12-20 01:46:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    function formatName(user) {
+      return user.firstName + ' ' + user.lastName;
+    }
+    function getGreeting(user) {
+      if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+      }
+      return <h1>Hello, Stranger.</h1>;
+    }
+    // function tick() {
+    //   const element = (
+    //     <div>
+    //       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    //     </div>
+    //   );
+    //   ReactDOM.render(element, document.getElementById('root'));
+    // }
+
+    // function handle_intervals(){
+    //   setInterval(tick, 6000);
+    // }
+    
+    const user = {
+      firstName: 'Harper',
+      lastName: 'Perez'
+    };
+    
+    const element = (
+      <div>
+         <h1>
+          Hello, {formatName(user)}!
+        </h1><hr></hr>
+        <h5>
+          Greetings from , {getGreeting()}
+        </h5>
+        {/* <h5>
+            { handle_intervals()}
+        </h5> */}
+      </div>
+    );
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          
+          {element}
+         <h1>Hello World</h1>
       </div>
     );
   }
